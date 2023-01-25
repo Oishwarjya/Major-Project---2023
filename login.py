6385c2bc578017ef64a14ee0fdd42b953e5102fc
OISHWARJYA BANERJEE <ohitsmeoishee@gmail.com>
2023-01-26 00:26:49 +0530

    Login page ver-1

--- login.py
+++ login.py
@@ -1,15 +1,3 @@
-"""import streamlit as st
-import time
-import pandas as pd
-
-
-col1, col2, col3 = st.columns([3,5,2])
-
-col1.markdown(" #Welcome to SRM PQH")
-col2.markdown(" Here is spme info on the app")
-
-col3.metric(label="Tempurature", value ="60 C", delta= "3")
-"""
 import base64
 import streamlit as st
 import plotly.express as px
@@ -29,14 +17,14 @@ page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://images.unsplash.com/photo-1589810264340-0ce27bfbf751?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80");
-background-size: 180%;
+background-size: 100%;
 background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
 [data-testid="stSidebar"] > div:first-child {{
 background-image: url("data:image/png;base64,{img}");
-background-position: center; 
+background-position: top left; 
 background-repeat: no-repeat;
 background-attachment: fixed;
 }}
@@ -51,11 +39,18 @@ right: 2rem;
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.title("SRM PLACEMENT QUERY HANDLER")
-st.sidebar.header("Information")
+st.sidebar.image("srm.png", use_column_width=True)
+st.sidebar.title("Information")
+
+
 
 with st.container():
     st.header("LOGIN")
     st.markdown(
         "Students, Admins and Department head logins"
     )
-    st.plotly_chart(px.scatter(df, x="sepal_width", y="sepal_length", color="species"))
+    with st.form("entry_form", clear_on_submit= True):
+        st.selectbox("Login Type", ("Student", "Admin", "Department"))
+        st.text_input(label="Username")  
+        st.text_input(label="Password")
+        submitted = st.form_submit_button("Confirm") 
