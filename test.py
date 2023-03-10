72aa96ad5d11e204f429b7130673088d1a119083
OISHWARJYA BANERJEE <ohitsmeoishee@gmail.com>
2023-03-10 08:09:18 +0530

    database design and test python script and instructions

--- test.py
+++ test.py
@@ -1,26 +1,23 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-from st_aggrid import AgGrid
 
-# Load AgGrid data
-data = pd.read_csv('your_data.csv')
-
-# Create Streamlit app
-st.title('AgGrid Data Visualization')
-st.write('Here is the AgGrid data:')
-
-# Display AgGrid table
-AgGrid(data)
-
-# Create chart
-fig, ax = plt.subplots()
-ax.bar(data['x'], data['y'])
-
-# Set chart parameters
-ax.set_title('Bar Chart')
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label') 
-
-# Display chart
-st.pyplot(fig)
+# Python program to read
+# json file
+  
+  
+import json
+  
+# Opening JSON file
+f = open('trial_data.json')
+  
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+  
+# Iterating through the json
+# list
+#for i in data:
+#    print(i)
+for i in data['resolvers']:
+    print(i["username"])
+  
+# Closing file
+f.close()
